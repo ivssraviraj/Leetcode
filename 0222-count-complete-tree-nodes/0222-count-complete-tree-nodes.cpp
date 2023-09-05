6d63bf21a78c64334f91b1103db9c80674714f5c
Venkata Subramanya Sri Raviraj Indraganti <111710961+ivssraviraj@users.noreply.github.com>
2023-09-04 17:39:23 -0700

    Time: 29 ms (50.14%), Space: 30.8 MB (42.05%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -13,6 +13,17 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
-        return countNodes(root->left) + countNodes(root->right) + 1;
+
+        int hl=0, hr=0;
+
+        TreeNode *l=root, *r=root;
+
+        while(l) {hl++;l=l->left;}
+
+        while(r) {hr++;r=r->right;}
+
+        if(hl==hr) return pow(2,hl)-1;
+
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };

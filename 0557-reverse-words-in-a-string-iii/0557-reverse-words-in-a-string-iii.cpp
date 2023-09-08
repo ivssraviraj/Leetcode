c0e008d52ed40ce15c62684b4eb2ae0d5bb0d9bb
Venkata Subramanya Sri Raviraj Indraganti <111710961+ivssraviraj@users.noreply.github.com>
2023-09-07 23:29:25 -0700

    Time: 14 ms (65.08%), Space: 9.6 MB (78.15%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,37 +1,56 @@
 class Solution {
 public:
-    vector<string> getWords(string s){
+//     vector<string> getWords(string s){
         
-        vector<string> ans;
+//         vector<string> ans;
         
-        string word;
-        stringstream ss(s);
+//         string word;
+//         stringstream ss(s);
         
-        while(ss >> word){
-            ans.push_back(word);
-        }
+//         while(ss >> word){
+//             ans.push_back(word);
+//         }
         
-        return ans;
-    }
-    string reverseWords(string s) {
+//         return ans;
+//     }
+//     string reverseWords(string s) {
         
-        vector<string > res = getWords(s);
+//         vector<string > res = getWords(s);
         
-        for(int i=0;i<res.size();i++){
-            string ss = res[i];
-            reverse(ss.begin(), ss.end());
-            res[i] = ss;
+//         for(int i=0;i<res.size();i++){
+//             string ss = res[i];
+//             reverse(ss.begin(), ss.end());
+//             res[i] = ss;
             
+//         }
+        
+//         string ans;
+        
+//         for(string ss : res){
+//             ans += ss;
+//             ans += " ";
+//         }
+//         ans.pop_back();
+//         return ans;
+        
+//     }
+    string reverseWords(string s) {
+        int lastSpaceIndex = -1;
+        int len = (int)s.size();
+        for (int strIndex = 0; strIndex <= len; strIndex++) {
+            if (strIndex == len || s[strIndex] == ' ') {
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = strIndex - 1;
+                while (startIndex < endIndex) {
+                    char temp = s[startIndex];
+                    s[startIndex] = s[endIndex];
+                    s[endIndex] = temp;
+                    startIndex++;
+                    endIndex--;
+                }
+                lastSpaceIndex = strIndex;
+            }
         }
-        
-        string ans;
-        
-        for(string ss : res){
-            ans += ss;
-            ans += " ";
-        }
-        ans.pop_back();
-        return ans;
-        
+        return s;
     }
 };

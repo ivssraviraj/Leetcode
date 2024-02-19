d849ba8c912f9b0ea7aca52548c6b7a362d4d372
Venkata Subramanya Sri Raviraj Indraganti <111710961+ivssraviraj@users.noreply.github.com>
2024-02-18 17:32:48 -0800

    Time: 0 ms, Memory: 7.4 MB - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        long l=0, r=x, mid;
+        int res = -1;
+        
+        while(l<=r){
+            
+            mid = l + (r-l)/2;
+            
+            if(mid*mid <= x){
+                
+             l = mid + 1;
+                res = mid;
+            }
+            else if(mid*mid > x) {
+                
+                r = mid -1;
+            }
+            
+           
+        }
+        return res;
+    }
+};

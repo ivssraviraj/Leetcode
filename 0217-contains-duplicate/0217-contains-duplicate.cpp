3dddaa2d1863825a97bc9caf7ee0ac0cf02aa2f9
Venkata Subramanya Sri Raviraj Indraganti <111710961+ivssraviraj@users.noreply.github.com>
2024-05-07 17:14:17 -0700

    Time: 90 ms (66.26%), Space: 73.1 MB (63.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> m;
+        unordered_map<int, int> m;
         
         for(int n : nums){
             if(m[n] > 0) return true;
